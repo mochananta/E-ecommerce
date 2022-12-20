469e7ae54fd957acf82719cf5f94872c39926747
Hamsyaff <angga10nugraha10@gmail.com>
2022-12-20 20:30:43 +0700

    admin delete category

--- app/Http/Livewire/Admin/AdminCategoriesComponent.php
+++ app/Http/Livewire/Admin/AdminCategoriesComponent.php
@@ -8,7 +8,15 @@ use Livewire\WithPagination;
 
 class AdminCategoriesComponent extends Component
 {
+    public $category_id;
     use WithPagination;
+
+    public function deleteCategory()
+    {
+        $category = Category::find($this->category_id);
+        $category->delete();
+        session()->flash('message', 'Category has been deleted successfully!');
+    }
     public function render()
     {
         $categories = Category::orderBy('name', 'ASC')->paginate(5);
