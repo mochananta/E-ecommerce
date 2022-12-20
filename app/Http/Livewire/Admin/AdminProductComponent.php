864eafabb5d34a0fef44ca4e3cf510f33ec270b8
Hamsyaff <angga10nugraha10@gmail.com>
2022-12-20 21:09:04 +0700

    admin product page

---
+++ app/Http/Livewire/Admin/AdminProductComponent.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use App\Models\Product;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class AdminProductComponent extends Component
+{
+    use WithPagination;
+
+    public function render()
+    {
+        $products = Product::orderBy('created_at', 'DESC')->paginate(6);
+        return view('livewire.admin.admin-product-component', ['products' => $products]);
+    }
+}
