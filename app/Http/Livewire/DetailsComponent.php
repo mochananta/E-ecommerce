c7cc9ec9f12c2eb37d4b34466c595c285a376e23
Hamsyaff <angga10nugraha10@gmail.com>
2022-12-20 07:51:01 +0700

    shopping cart

--- app/Http/Livewire/DetailsComponent.php
+++ app/Http/Livewire/DetailsComponent.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use App\Models\Product;
 use Livewire\Component;
+use cart;
 
 class DetailsComponent extends Component
 {
@@ -12,6 +13,12 @@ class DetailsComponent extends Component
     public function mount($slug)
     {
         $this->slug = $slug;
+    }
+    public function store($product_id, $product_name, $product_price)
+    {
+        Cart::add($product_id, $product_name, 1, $product_price)->associate('\App\Models\Product');
+        session()->flash('success_message', 'Item added in Cart');
+        return redirect()->route('shop.cart');
     }
     public function render()
     {
