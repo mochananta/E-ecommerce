83c4279e9e66ef4d77f1ad3c566e3aea6c8629de
Hamsyaff <angga10nugraha10@gmail.com>
2022-12-20 09:34:03 +0700

    sorting

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -10,6 +10,8 @@ use Cart;
 class ShopComponent extends Component
 {
     use WithPagination;
+    public $pageSize = 12;
+    public $orderBy = "Default Sorting";
 
     public function store($product_id, $product_name, $product_price)
     {
@@ -17,9 +19,28 @@ class ShopComponent extends Component
         session()->flash('success_message', 'Item added in Cart');
         return redirect()->route('shop.cart');
     }
+    public function changePageSize($size)
+    {
+        $this->pageSize = $size;
+    }
+
+    public function changeOrderBy($order)
+    {
+        $this->orderBy = $order;
+    }
+
     public function render()
     {
-        $products = Product::paginate(12);
+        if ($this->orderBy == 'Price: Low to High') {
+            $products = Product::orderBy('regular_price', 'ASC')->paginate($this->pageSize);
+        } else if ($this->orderBy == 'Price: High to Low') {
+            $products = Product::orderBy('regular_price', 'DESC')->paginate($this->pageSize);
+        } else if ($this->orderBy == 'Sort By Newness') {
+            $products = Product::orderBy('created_at', 'DESC')->paginate($this->pageSize);
+        } else {
+            $products = Product::paginate($this->pageSize);
+        }
+
         return view('livewire.shop-component', ['products' => $products]);
     }
 }
